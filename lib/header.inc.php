a14db06c395b7acdd6e2f2f4ace2c4cb5dabd35b
Jacob Salomón <jacsalomon@yahoo.com>
2010-08-31 11:34:21 -0500

    Corrección del encabezado
    Corrección del "merged" con la versión en "upstream".

--- lib/header.inc.php
+++ lib/header.inc.php
@@ -1,12 +1,6 @@
-<<<<<<< HEAD:lib/header.inc.php
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML-1.0 Strict//EN"
-	http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmls="http://www.w3.org/1999/xhtml" xml:lang="es" lang:"es">
-=======
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
->>>>>>> 5896e89aa32bfca634c8379ca6208fb4657e9eb5:lib/header.inc.php
     <head>
         <!--Meta tags -->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
